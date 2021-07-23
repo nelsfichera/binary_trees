51b49a571b840d675fa20c1f2fd86b861959ba95
Nels <nelsfichera@gmail.com>
2021-07-23 09:20:20 -0400

    16 fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,38 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - count the parents at given node of binary tree.
- * @tree: binary tree.
- * Return: depth of binary tree.
- */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t parent = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	parent = binary_tree_depth(tree->parent);
-	if (!tree->parent)
-		return (parent);
-	return (parent + 1);
-}
-/**
- * perfection - recursive function to test if tree is perfect
- * @tree: binary tree.
- * @depth: depth of tree.
- * @level: level of tree.
- * Return: 1 on success 0 on failure.
- */
-int perfection(const binary_tree_t *tree, size_t depth, size_t level)
-{
-	if (!tree->left && !tree->right)
-		return (depth == level + 1);
-	if (!tree->left || !tree->right)
-		return (0);
-	return (perfection(tree->left, depth, level + 1) &&
-		perfection(tree->right, depth, level + 1));
-}
-/**
  * binary_tree_is_perfect - checks if binary tree is perfect.
  * @tree: binary tree.
  * Return: 1 on success 0 on failure.
@@ -43,9 +10,35 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	depth = binary_tree_depth(tree);
 
+	if (tree == NULL)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+		return (0);
+	return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+}
+/**
+ * binary_tree_height - height of binary tree.
+ * @tree: binary tree.
+ * Return: height of binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_size = 0, right_size = 0;
+
 	if (!tree)
 		return (0);
-	if (perfection(tree, depth, 0))
-		return (1);
-	return (0);
+	else if (tree->left || tree->right)
+	{
+		left_size = binary_tree_height(tree->left);
+		right_size = binary_tree_height(tree->right);
+		if (left_size > right_size)
+			return (left_size + 1);
+		return (right_size + 1);
+	}
+	else
+		return (0);
 }
+
